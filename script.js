62ef533f0bb71657823477640ff67aec79292cf4
Lucy-Silva <lucyellen29silva@gmail.com>
2022-05-14 17:01:08 -0300

    aula javascript

---
+++ script.js
@@ -0,0 +1,38 @@
+ let carro="volvo";
+ //alert("Alô Mundo!");
+ let a=10;
+ let b=5;
+ //alert (a+b);
+ function cliqueBotao(){
+     alert("Você clicou em um botão");
+ }
+let clique=0;
+ 
+function contarClique(){
+    clique++;
+    //document.getElementById("resultado").innerHTML="O contador está com <mark>">+clique+" </mark> cliques";
+    document.getElementById("resultado").innerHTML=clique;
+}
+
+function mediaDoAluno(){
+    let nota01 = prompt("Digite sua primeira nota");
+    let nota02 = prompt("Digite sua segunda nota");
+    
+    let Media=(nota01+nota02)/2;
+}
+
+let n = prompt("Digite um número");
+let total= n/2;
+let resultado 2 == 0 ?"Par":"Impar";
+alert(n +"é" + resultado);
+
+function qual é maior(){
+ let n = prompt("Digite um número");
+ let resultado 2 == 0 ?"A é maior que 8";
+ alert(n +"é" + resultado);
+}
+
+    
+
+
+ 
